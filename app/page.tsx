3b71c9bfa081cd1fe5db800669f5caa3337719b0
PacificD <1484636442@qq.com>
2023-06-05 22:33:40 +0800

    feat(app): clerk sign-in

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-import { UserButton } from '@clerk/nextjs'
+import { UserButton, auth, currentUser } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth()
+  const user = await currentUser()
+
+  if (!user) return <div>Not logged in</div>
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div>User Id: {userId}</div>
+      <div>Hello {user?.firstName}</div>
       <div>
         <UserButton afterSignOutUrl='/' />
       </div>
