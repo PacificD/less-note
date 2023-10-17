212d409f9bc2faf468921c883ee4ab01d68237de
PacificD <1484636442@qq.com>
2023-10-18 00:33:20 +0800

    feat(constants): documents constants

---
+++ constants/documents.ts
@@ -0,0 +1,7 @@
+export const CREATE_DOCUMENT_DEFAULT_SONNER = {
+  loading: 'Creating a new note...',
+  success: 'New note created!',
+  error: 'Failed to create a new note.'
+}
+
+export const CREATE_DOCUMENT_DEFAULT_ARGS = { title: 'Untitled' }
